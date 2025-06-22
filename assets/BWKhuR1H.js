ea895673977f9c8154c845922af51c83f7b87844
Akash Singh1 <akashqsingh89@gmail.com>
2025-06-22 17:02:32 +0530

    Updates

--- assets/BWKhuR1H.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./CGqw2bqD.js";const o=s("/assets/imgs/svg-assets/arrow-right-top.svg");export{o as _};
---
+++ assets/CBqOoBMr.js
@@ -0,0 +1,1 @@
+import{_ as n,a as t}from"./CMlk2kdE.js";import{u as a}from"./CCvwNuV1.js";import{c,o as m,b as e,Z as l,F as _}from"./DwO-rMf-.js";const g={__name:"preview",setup(p){return a({link:[{rel:"stylesheet",href:"/landing-preview/css/preview-style.css"}]}),(s,i)=>{const o=n,r=t;return m(),c(_,null,[e(o),e(r),l(s.$slots,"default")],64)}}};export{g as default};
