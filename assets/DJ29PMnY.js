3f1fabc858c08928c3951b68502d9212faabba47
Akash Singh1 <akashqsingh89@gmail.com>
2025-06-22 16:00:35 +0530

    Updates

--- assets/DJ29PMnY.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./Cf_yjBzr.js";const p=s("/assets/vid/vid-startup.mp4");export{p as _};
---
+++ assets/Dml5kDjx.js
@@ -0,0 +1,1 @@
+import{_ as n,a as t}from"./DoHKJ_8y.js";import{u as a}from"./DVco4fI4.js";import{c,o as m,b as e,Z as l,F as _}from"./DsbVHxhb.js";const g={__name:"preview",setup(p){return a({link:[{rel:"stylesheet",href:"/landing-preview/css/preview-style.css"}]}),(s,i)=>{const o=n,r=t;return m(),c(_,null,[e(o),e(r),l(s.$slots,"default")],64)}}};export{g as default};
