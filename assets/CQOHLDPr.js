276f030e05554a4598411afa8c245683d7e7b75a
Akash Singh1 <akashqsingh89@gmail.com>
2025-06-27 23:57:57 +0530

    Updates

---
+++ assets/CQOHLDPr.js
@@ -0,0 +1,1 @@
+import{p as s}from"./3mGDi95K.js";const o=s("/assets/imgs/svg-assets/arrow-right-top.svg");export{o as _};
--- assets/DmvaDwz_.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,a as t}from"./BZRXDLYw.js";import{u as a}from"./CJvhMZTm.js";import{c,o as m,b as e,Z as l,F as _}from"./8IRDx0AK.js";const g={__name:"preview",setup(p){return a({link:[{rel:"stylesheet",href:"/landing-preview/css/preview-style.css"}]}),(s,i)=>{const o=n,r=t;return m(),c(_,null,[e(o),e(r),l(s.$slots,"default")],64)}}};export{g as default};
