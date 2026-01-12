d2278638e2425db3969a26926f474407db2afa92
Akash Singh <akashqsingh365@gmail.com>
2026-01-12 23:59:11 +0530

    Updates

--- assets/BcPL2Ez4.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./CJR5LoMV.js";const o=s("/assets/imgs/svg-assets/arrow-right-top.svg");export{o as _};
--- assets/lvZb-ylD.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,a as n}from"./DHrFZwfo.js";import{u as t}from"./Ba4-sgjN.js";import{c,o as m,b as e,a3 as l,F as _}from"./CJR5LoMV.js";const g={__name:"preview",setup(p){return t({link:[{rel:"stylesheet",href:"/landing-preview/css/preview-style.css"}]}),(s,i)=>{const o=a,r=n;return m(),c(_,null,[e(o),e(r),l(s.$slots,"default")],64)}}};export{g as default};
